b53f665de7a564df26503f657f683317f68f33fe
particle4dev <particle4dev@gmail.com>
2015-07-03 07:23:35 +0700

    add react package

--- client/views/pages/project/ProjectPage.jsx
+++ client/views/pages/project/ProjectPage.jsx
@@ -1,40 +1,39 @@
-Project = ReactMeteor.createClass({
-    templateName: "project",
-    componentDidMount: function() {
-        
-    },
-    getMeteorState: function() {
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+Project = React.createClass({
+    componentDidMount: function() {},
+    mixins: [ReactMeteorData],
+    getMeteorData () {
         return {
             todos: TodoListsCollection.find({}, {sort: {updatedAt: -1}}).fetch()
-        }
+        };
     },
-    renderTodo: function(model) {
-        return <TodoLists 
+    renderTodo (model) {
+        return <TodoLists
             key={model._id}
             model={model}
-            idTodo={model._id} 
-            todo={model.todo}/>;
-    },
-    openModal: function() {
+            idTodo={model._id}
+            todo={model.todo}
+            isSimulation={model.isSimulation}/>;
+   },
+    openModal () {
         $('#modal1').openModal({
             dismissible: true, // Modal can be dismissed by clicking outside of the modal
-            opacity: .5, // Opacity of modal background
+            opacity: 0.5, // Opacity of modal background
             in_duration: 500, // Transition in duration
         });
     },
-    createNewTodo: function(evt) {
+    createNewTodo (evt) {
         evt.preventDefault();
         var self = this;
         var todo = React.findDOMNode(this.refs.todo).value.trim();
         if(!todo) {
-                return;
+            return;
         }
         Meteor.call('insertNewTodo', {todo: todo, timeTodo: new Date()}, function(err, res){
-            if(err){
-                console.log(err)
-            }
-            else{
-                console.log(res)
+            console.log(err, res);
+            if(err){}
+            else {
                 React.findDOMNode(self.refs.todo).value = '';
                 $('#modal1').closeModal({
                     out_duration: 500, // Transition out duration
@@ -43,7 +42,7 @@ Project = ReactMeteor.createClass({
         });
         return;
     },
-    render: function() {
+    render () {
         return (
             <div id="container" className="container" style={{padding: "0px", marginBottom: "80px", fontFamily: "Shadows Into Light"}}>
                 <div className="header" style={{padding: "25px 0px"}}>
@@ -59,9 +58,11 @@ Project = ReactMeteor.createClass({
                     </a>
                     <div className="row" style={{height: "40px"}}>
                     </div>
-                    {this.state.todos.map(this.renderTodo)}
+                    <ReactCSSTransitionGroup transitionName="example">
+                      {this.data.todos.map(this.renderTodo)}
+                    </ReactCSSTransitionGroup>
                 </div>
-                
+
                 <div id="modal1" className="modal">
                     <div className="modal-content" style={{padding: "10px"}}>
                         <form>
@@ -82,113 +83,5 @@ Project = ReactMeteor.createClass({
                 </div>
             </div>
         );
-    }
-});
-
-var TodoLists = ReactMeteor.createClass({
-    templateName: "todoLists",
-    componentDidMount: function() {
-        $(".dropdown-button").dropdown();
-    },
-    handleCloseTodo: function(evt) {
-        evt.preventDefault();
-        Meteor.call('removeTodo', this.props.idTodo, function(err, res){
-            if(err){
-                console.log(err)
-            }
-            else {
-                console.log(res);
-            }
-        });
-    },
-    handleUpdateTodo: function(evt) {
-        evt.preventDefault();
-        var self = this;
-        var todo = React.findDOMNode(this.refs.todo).value.trim();
-        if(!todo) {
-                return;
-        }
-        Meteor.call('updateTodo', {idTodo: this.props.idTodo, todo: todo}, function(err, res){
-            if(err){
-                console.log(err)
-            }
-            else{
-                console.log(res);
-                $("#updateTodo"+self.props.idTodo).closeModal({
-                    out_duration: 500, // Transition out duration
-                });
-            }
-        });
-        return;
-    },
-    openModalCloseTodo: function() {
-        $('#closeTodo'+this.props.idTodo).openModal({
-            dismissible: true, // Modal can be dismissed by clicking outside of the modal
-            opacity: .5, // Opacity of modal background
-            in_duration: 500, // Transition in duration
-        });
-    },
-    openModalUpdateTodo: function() {
-        $('#updateTodo'+this.props.idTodo).openModal({
-            dismissible: true, // Modal can be dismissed by clicking outside of the modal
-            opacity: .5, // Opacity of modal background
-            in_duration: 500, // Transition in duration
-        });
-    },
-    render: function(){
-        return (
-            <div className="row">
-                <div className="col s10" style={{
-                    position: "relative",
-                    paddingLeft: "40px",
-                }}>
-                    <input type="checkbox" id={this.props.idTodo} />
-                    <label htmlFor={this.props.idTodo} style={{display: "inline", fontSize: "1.3rem", position: "absolute", left: "10px"}}></label>
-                    <span style={{wordWrap: "break-word", fontSize: "18px", lineHeight: "18px"}}>{this.props.todo}</span>
-                </div>
-                <div className="col s2" style={{
-                    lineHeight: "1",
-                    fontSize: "24px"
-                }}>
-                    <a className="dropdown-button right" data-activates={"dropdown"+this.props.idTodo}><i className="mdi-navigation-more-vert" style={{color: "#fff"}}></i></a>
-                    <ul id={"dropdown"+this.props.idTodo} className="dropdown-content">
-                        <li className="modal-trigger" onClick={this.openModalCloseTodo}><a >Delete</a></li>
-                        <li className="divider"></li>
-                        <li className="modal-trigger" onClick={this.openModalUpdateTodo}><a >Archive</a></li>
-                    </ul>
-                </div>                
-                <div id={"closeTodo"+this.props.idTodo} className="modal">
-                    <div className="modal-content" style={{padding: "10px", color:"#26a69a"}}>
-                        <h5>Are you sure you want to delete this todo?</h5>
-                    </div>
-                    <div className="modal-footer">
-                        <a className="waves-effect modal-close waves-green btn-flat" style={{color: "green", padding: "0 1rem"}} onClick={this.handleCloseTodo}>
-                            <i className="mdi-action-done" style={{fontSize: "25px"}}></i>
-                        </a>
-                        <a className=" modal-action modal-close waves-effect waves-green btn-flat" style={{color: "red", padding: "0 1rem"}}>
-                            <i className="mdi-content-clear" style={{fontSize: "25px"}}></i>
-                        </a>
-                    </div>
-                </div>
-                <div id={"updateTodo"+this.props.idTodo} className="modal bottom-sheet">
-                    <div className="modal-content" style={{padding: "10px", color:"#26a69a"}}>
-                        <form>
-                            <div className="input-field">
-                                <textarea id="textarea2" className="materialize-textarea" ref="todo" style={{color: "#26a69a"}}>{this.props.todo}</textarea>
-                                <label for="textarea2"></label>
-                            </div>
-                        </form>
-                    </div>
-                    <div className="modal-footer">
-                        <a className="waves-effect waves-green btn-flat" style={{color: "green", padding: "0 1rem"}} onClick={this.handleUpdateTodo}>
-                            <i className="mdi-action-done" style={{fontSize: "25px"}}></i>
-                        </a>
-                        <a className=" modal-action modal-close waves-effect waves-green btn-flat" style={{color: "red", padding: "0 1rem"}}>
-                            <i className="mdi-content-clear" style={{fontSize: "25px"}}></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        )
     }
 });
---
+++ client/views/pages/project/TodoLists.jsx
@@ -0,0 +1,101 @@
+TodoLists = React.createClass({
+    componentDidMount: function() {
+        $(".dropdown-button").dropdown();
+    },
+    handleCloseTodo: function(evt) {
+        evt.preventDefault();
+        Meteor.call('removeTodo', this.props.idTodo, function(err, res){
+            console.log(err, res);
+            if(err){}
+            else {}
+        });
+    },
+    handleUpdateTodo: function(evt) {
+        evt.preventDefault();
+        var self = this;
+        var todo = React.findDOMNode(this.refs.todo).value.trim();
+        if(!todo) {
+          return;
+        }
+        Meteor.call('updateTodo', {idTodo: this.props.idTodo, todo: todo}, function(err, res){
+            console.log(err, res);
+            if(err){}
+            else{
+                $("#updateTodo" + self.props.idTodo).closeModal({
+                    out_duration: 500, // Transition out duration
+                });
+            }
+        });
+        return;
+    },
+    openModalCloseTodo: function() {
+        $('#closeTodo' + this.props.idTodo).openModal({
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: 0.5, // Opacity of modal background
+            in_duration: 500, // Transition in duration
+        });
+    },
+    openModalUpdateTodo: function() {
+        $('#updateTodo' + this.props.idTodo).openModal({
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: 0.5, // Opacity of modal background
+            in_duration: 500, // Transition in duration
+        });
+    },
+    render: function(){
+        return (
+            <div className="row">
+                <div className="col s10" style={{
+                    position: "relative",
+                    paddingLeft: "40px",
+                }}>
+                    <input type="checkbox" id={this.props.idTodo} />
+                    <label htmlFor={this.props.idTodo} style={{display: "inline", fontSize: "1.3rem", position: "absolute", left: "10px"}}></label>
+                    <span style={{wordWrap: "break-word", fontSize: "18px", lineHeight: "18px"}}>{this.props.todo} {"" + this.props.isSimulation}</span>
+                </div>
+                <div className="col s2" style={{
+                    lineHeight: "1",
+                    fontSize: "24px"
+                }}>
+                    <a className="dropdown-button right" data-activates={"dropdown"+this.props.idTodo}><i className="mdi-navigation-more-vert" style={{color: "#fff"}}></i></a>
+                    <ul id={"dropdown"+this.props.idTodo} className="dropdown-content">
+                        <li className="modal-trigger" onClick={this.openModalCloseTodo}><a >Delete</a></li>
+                        <li className="divider"></li>
+                        <li className="modal-trigger" onClick={this.openModalUpdateTodo}><a >Archive</a></li>
+                    </ul>
+                </div>
+                <div id={"closeTodo"+this.props.idTodo} className="modal">
+                    <div className="modal-content" style={{padding: "10px", color:"#26a69a"}}>
+                        <h5>Are you sure you want to delete this todo?</h5>
+                    </div>
+                    <div className="modal-footer">
+                        <a className="waves-effect modal-close waves-green btn-flat" style={{color: "green", padding: "0 1rem"}} onClick={this.handleCloseTodo}>
+                            <i className="mdi-action-done" style={{fontSize: "25px"}}></i>
+                        </a>
+                        <a className=" modal-action modal-close waves-effect waves-green btn-flat" style={{color: "red", padding: "0 1rem"}}>
+                            <i className="mdi-content-clear" style={{fontSize: "25px"}}></i>
+                        </a>
+                    </div>
+                </div>
+                <div id={"updateTodo"+this.props.idTodo} className="modal bottom-sheet">
+                    <div className="modal-content" style={{padding: "10px", color:"#26a69a"}}>
+                        <form>
+                            <div className="input-field">
+                                <textarea id="textarea2" className="materialize-textarea" ref="todo" style={{color: "#26a69a"}}>{this.props.todo}</textarea>
+                                <label for="textarea2"></label>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="modal-footer">
+                        <a className="waves-effect waves-green btn-flat" style={{color: "green", padding: "0 1rem"}} onClick={this.handleUpdateTodo}>
+                            <i className="mdi-action-done" style={{fontSize: "25px"}}></i>
+                        </a>
+                        <a className=" modal-action modal-close waves-effect waves-green btn-flat" style={{color: "red", padding: "0 1rem"}}>
+                            <i className="mdi-content-clear" style={{fontSize: "25px"}}></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
